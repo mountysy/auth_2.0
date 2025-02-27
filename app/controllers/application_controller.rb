eee6236069c8ad617b7ba7b63b1933272321fb37
Shan Yan <shan.yan@kellogg.northwestern.edu>
2025-02-27 19:46:12 +0000

    class 8 2nd try

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
   def current_user
     puts "----- this code runs at the beginning of the request"
     # find logged-in user
+    @current_user = User.find_by({"id" => session["user_id"]})
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,26 @@ class SessionsController < ApplicationController
     # 2. if the user exists -> check if they know their password
     # 3. if they know their password -> login is successful
     # 4. if the user doesn't exist or they don't know their password -> login fails
-    flash["notice"] = "Welcome."
-    redirect_to "/companies"
+    @user = User.find_by({"email" => params["email"]})
+
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome."
+        redirect_to "/companies"
+      else 
+        flash["notice"] = "Nope"
+        redirect_to "/login"
+      end
+    else 
+      flash["notice"] = "Nope"
+      redirect_to "/login"
+    end 
   end
 
   def destroy
     # logout the user
+    session["user_id"] = nil
     flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
